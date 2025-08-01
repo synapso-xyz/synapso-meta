c6bff4ce3b53c9db59e1fb83dbd18c032b9f6dac
Ganesh Palanikumar <140663473+ganesh-palanikumar@users.noreply.github.com>
2025-08-01 11:06:19 -0700

    Update setup.py - Release v0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="synapso",
-    version="0.3.1",
+    version="0.3.2",
     description="Meta-package that installs synapso-core and synapso-cli.",
     author="Ganesh Palanikumar",
     author_email="me@ganeshpalanikumar.dev",
